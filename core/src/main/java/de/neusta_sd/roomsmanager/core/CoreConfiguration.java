4fe77f81140907ebe5b8aee6e921e891fd3ba1a0
Adrian Tello Lasheras <a.lasheras@neusta.de>
2017-11-29 22:57:33 +0100

    Configure jpa directories

--- core/src/main/java/de/neusta_sd/roomsmanager/core/CoreConfiguration.java
+++ core/src/main/java/de/neusta_sd/roomsmanager/core/CoreConfiguration.java
@@ -1,7 +1,13 @@
 package de.neusta_sd.roomsmanager.core;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories("de.neusta_sd.roomsmanager.core.entities.repositories")
+@EntityScan("de.neusta_sd.roomsmanager.core.entities")
+@ComponentScan("de.neusta_sd.roomsmanager.core")
 public class CoreConfiguration {
 }
