824782b4ae79ba10f54fd93da089d753c4cb0141
Adrian Tello Lasheras <a.lasheras@neusta.de>
2017-11-29 23:05:26 +0100

    Create name addition entity and repository.

---
+++ core/src/main/java/de/neusta_sd/roomsmanager/core/entities/NameAddition.java
@@ -0,0 +1,32 @@
+package de.neusta_sd.roomsmanager.core.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created by Adrian Tello on 29/11/2017.
+ */
+@Entity
+public class NameAddition {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 10, nullable = false)
+    private String name;
+
+    public NameAddition() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ core/src/main/java/de/neusta_sd/roomsmanager/core/entities/repositories/NameAdditionRepository.java
@@ -0,0 +1,10 @@
+package de.neusta_sd.roomsmanager.core.entities.repositories;
+
+import de.neusta_sd.roomsmanager.core.entities.NameAddition;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Adrian Tello on 29/11/2017.
+ */
+public interface NameAdditionRepository extends CrudRepository<NameAddition, Long> {
+}
