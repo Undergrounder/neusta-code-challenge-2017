46dad5fed9a72c5f90e538b2ed6465921ee4fc69
Adrian Tello Lasheras <a.lasheras@neusta.de>
2017-11-29 23:02:32 +0100

    Create title entity and repository.

---
+++ core/src/main/java/de/neusta_sd/roomsmanager/core/entities/Title.java
@@ -0,0 +1,32 @@
+package de.neusta_sd.roomsmanager.core.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created by Adrian Tello on 29/11/2017.
+ */
+@Entity
+public class Title {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    public Title() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
