7e5e3e4483c6eaacc5bc18906bfcef1c88b6d8da
Adrian Tello Lasheras <a.lasheras@neusta.de>
2017-12-01 21:34:54 +0100

    Enable transaction management

--- core/src/main/java/de/neusta_sd/roomsmanager/core/CoreConfiguration.java
+++ core/src/main/java/de/neusta_sd/roomsmanager/core/CoreConfiguration.java
@@ -4,9 +4,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories("de.neusta_sd.roomsmanager.core.entities.repositories")
+@EnableTransactionManagement
 @EntityScan("de.neusta_sd.roomsmanager.core.entities")
 @ComponentScan("de.neusta_sd.roomsmanager.core")
 public class CoreConfiguration {
