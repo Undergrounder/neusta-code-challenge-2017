f2a7cab6155e9865b2b8b60992c9cac6c1f7a69a
Adrian Tello Lasheras <a.lasheras@neusta.de>
2017-12-01 12:48:32 +0100

    Initialization data

---
+++ core/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+-------------------------------------------------------------------------------
+-- Initial data
+-------------------------------------------------------------------------------
+-- Title
+INSERT INTO title (name) VALUES ('Dr.')
+
+-------------------------------------------------------------------------------
+-- NameAddition
+INSERT INTO name_addition (name) VALUES ('van')
+INSERT INTO name_addition (name) VALUES ('von')
+INSERT INTO name_addition (name) VALUES ('de')
