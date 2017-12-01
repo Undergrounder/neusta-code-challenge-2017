b2a208b53b88a839bb51c143e224743c9958ce79
Adrian Tello Lasheras <a.lasheras@neusta.de>
2017-12-01 12:29:46 +0100

    Create fatjar

---
+++ fatjar/src/main/java/de/neusta_sd/roomsmanager/fatjar/FatJarApplication.java
@@ -0,0 +1,17 @@
+package de.neusta_sd.roomsmanager.fatjar;
+
+import de.neusta_sd.roomsmanager.frontend.FrontendConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Created by Adrian Tello on 01/12/2017.
+ */
+@SpringBootApplication
+@Import(FrontendConfiguration.class)
+public class FatJarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FatJarApplication.class, args);
+    }
+}
