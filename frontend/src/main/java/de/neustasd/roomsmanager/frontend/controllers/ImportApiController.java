ed7940e030ce946fe14b936d862b60966dbe9148
Adrian Tello Lasheras <a.lasheras@neusta.de>
2017-12-16 20:09:21 +0100

    cehckstyle

--- frontend/src/main/java/de/neustasd/roomsmanager/frontend/controllers/ImportApiController.java
+++ frontend/src/main/java/de/neustasd/roomsmanager/frontend/controllers/ImportApiController.java
@@ -25,66 +25,67 @@ import org.springframework.web.multipart.support.MissingServletRequestPartExcept
 @RequestMapping(BASE_PATH)
 public class ImportApiController extends AbstractApiController {
 
-    public static final String BASE_PATH = ApiController.BASE_PATH + "/import";
+  public static final String BASE_PATH = ApiController.BASE_PATH + "/import";
 
-    private final ImportFacade importFacade;
+  private final ImportFacade importFacade;
 
-    public ImportApiController(final ImportFacade importFacade) {
-        this.importFacade = importFacade;
+  public ImportApiController(final ImportFacade importFacade) {
+    this.importFacade = importFacade;
+  }
+
+  @RequestMapping(
+    consumes = "multipart/form-data",
+    method = RequestMethod.POST,
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  @ResponseStatus(HttpStatus.ACCEPTED)
+  public ImportResultDto postImport(@RequestParam("file") MultipartFile file)
+      throws IOException, ImportFacade.ImportException {
+    try (InputStream fileInputStream = file.getInputStream()) {
+      return getImportFacade().importStream(fileInputStream);
+    }
+  }
+
+  @RequestMapping
+  public void othersImport() throws MethodNotAllowedException {
+    throw new MethodNotAllowedException();
+  }
+
+  @ExceptionHandler(value = {Exception.class})
+  @Override
+  public ResponseEntity<ExceptionDto> exceptionHandler(Exception e) {
+    if (e instanceof MissingServletRequestPartException) {
+      return createExceptionResponseEntity(400, 400, e.getMessage());
+    } else if (e instanceof ImportFacade.ImportValidationFailedException) {
+      final ImportFacade.ImportValidationFailedException importValidationFailedException =
+          (ImportFacade.ImportValidationFailedException) e;
+      final int bodyCode = getBodyCodeForException(importValidationFailedException);
+
+      return createExceptionResponseEntity(400, bodyCode, e.getMessage());
+    }
+    return super.exceptionHandler(e);
+  }
+
+  private int getBodyCodeForException(ImportFacade.ImportValidationFailedException e) {
+    int code = 400;
+    switch (e.getFailedValidation()) {
+      case DUPLICATED_ROOM_NUMBER:
+        code = 2;
+        break;
+      case DUPLICATED_PERSON:
+        code = 3;
+        break;
+      case INVALID_ENTRY:
+        code = 4;
+        break;
+      default:
+        // code = 400
     }
 
-    @RequestMapping(
-      consumes = "multipart/form-data",
-      method = RequestMethod.POST,
-      produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public ImportResultDto postImport(@RequestParam("file") MultipartFile file)
-        throws IOException, ImportFacade.ImportException {
-        try (InputStream fileInputStream = file.getInputStream()) {
-            return getImportFacade().importStream(fileInputStream);
-        }
-    }
+    return code;
+  }
 
-    @RequestMapping
-    public void othersImport() throws MethodNotAllowedException {
-        throw new MethodNotAllowedException();
-    }
-
-    @ExceptionHandler(value = {Exception.class})
-    @Override
-    public ResponseEntity<ExceptionDto> exceptionHandler(Exception e) {
-        if (e instanceof MissingServletRequestPartException) {
-            return createExceptionResponseEntity(400, 400, e.getMessage());
-        } else if (e instanceof ImportFacade.ImportValidationFailedException) {
-            final ImportFacade.ImportValidationFailedException importValidationFailedException =
-                (ImportFacade.ImportValidationFailedException) e;
-            final int bodyCode = getBodyCodeForException(importValidationFailedException);
-
-            return createExceptionResponseEntity(400, bodyCode, e.getMessage());
-        }
-        return super.exceptionHandler(e);}
-
-    private int getBodyCodeForException(ImportFacade.ImportValidationFailedException e) {
-        int code = 400;
-        switch (e.getFailedValidation()) {
-            case DUPLICATED_ROOM_NUMBER:
-                code = 2;
-                break;
-            case DUPLICATED_PERSON:
-                code = 3;
-                break;
-            case INVALID_ENTRY:
-                code = 4;
-                break;
-            default:
-                // code = 400
-        }
-
-        return code;
-    }
-
-    private ImportFacade getImportFacade() {
-        return importFacade;
-    }
+  private ImportFacade getImportFacade() {
+    return importFacade;
+  }
 }
