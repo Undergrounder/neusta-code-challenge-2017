66c62fea6873ea253c399b3d2d294ff99aa8aa43
Adrian Tello Lasheras <a.lasheras@neusta.de>
2017-12-16 18:57:32 +0100

    Reformat PersonStringConverterImplUnitTest

--- facades/src/test/java/de/neustasd/roomsmanager/facades/imprt/csv/parser/converter/impl/PersonStringConverterImplUnitTest.java
+++ facades/src/test/java/de/neustasd/roomsmanager/facades/imprt/csv/parser/converter/impl/PersonStringConverterImplUnitTest.java
@@ -19,120 +19,120 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class PersonStringConverterImplUnitTest {
 
-  @Mock private TitleService titleService;
+    @Mock private TitleService titleService;
 
-  @Mock private NameAdditionService nameAdditionService;
+    @Mock private NameAdditionService nameAdditionService;
 
-  private PersonStringConverter personStringConverter;
+    private PersonStringConverter personStringConverter;
 
-  @Before
-  public void setUp() throws Exception {
-    personStringConverter = new PersonStringConverterImpl(titleService, nameAdditionService);
-  }
+    @Before
+    public void setUp() throws Exception {
+        personStringConverter = new PersonStringConverterImpl(titleService, nameAdditionService);
+    }
 
-  @Test(expected = Converter.ConversionException.class)
-  public void testConvertNull() {
-    // Test
-    personStringConverter.convert(null);
-  }
+    @Test(expected = Converter.ConversionException.class)
+    public void testConvertNull() {
+        // Test
+        personStringConverter.convert(null);
+    }
 
-  @Test(expected = Converter.ConversionException.class)
-  public void testConvertEmpty() {
-    // Test
-    personStringConverter.convert(null);
-  }
+    @Test(expected = Converter.ConversionException.class)
+    public void testConvertEmpty() {
+        // Test
+        personStringConverter.convert(null);
+    }
 
-  @Test
-  public void testConvertDennisFischer() {
-    // Prepare
-    final String personString = "Dennis Fischer (dfischer)";
+    @Test
+    public void testConvertDennisFischer() {
+        // Prepare
+        final String personString = "Dennis Fischer (dfischer)";
 
-    // Test
-    ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
+        // Test
+        final ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
 
-    // Verify
-    assertConversion(csvPersonData, null, "Dennis", null, "Fischer", "dfischer");
-  }
+        // Verify
+        assertConversion(csvPersonData, null, "Dennis", null, "Fischer", "dfischer");
+    }
 
-  @Test
-  public void testConvertIftikarAhmadKhan() {
-    // Prepare
-    final String personString = "Iftikar Ahmad Khan (ikhan)";
+    @Test
+    public void testConvertIftikarAhmadKhan() {
+        // Prepare
+        final String personString = "Iftikar Ahmad Khan (ikhan)";
 
-    // Test
-    ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
+        // Test
+        final ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
 
-    // Verify
-    assertConversion(csvPersonData, null, "Iftikar Ahmad", null, "Khan", "ikhan");
-  }
+        // Verify
+        assertConversion(csvPersonData, null, "Iftikar Ahmad", null, "Khan", "ikhan");
+    }
 
-  @Test
-  public void testConvertAlexanderJamesCole() {
-    // Prepare
-    final String personString = "Alexander James Cole (acole)";
+    @Test
+    public void testConvertAlexanderJamesCole() {
+        // Prepare
+        final String personString = "Alexander James Cole (acole)";
 
-    // Test
-    ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
+        // Test
+        final ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
 
-    // Verify
-    assertConversion(csvPersonData, null, "Alexander James", null, "Cole", "acole");
-  }
+        // Verify
+        assertConversion(csvPersonData, null, "Alexander James", null, "Cole", "acole");
+    }
 
-  @Test
-  public void testConvertThomasVonGostomski() {
-    // Prepare
-    final String personString = "Thomas von Gostomski (tgostomski)";
+    @Test
+    public void testConvertThomasVonGostomski() {
+        // Prepare
+        final String personString = "Thomas von Gostomski (tgostomski)";
 
-    when(nameAdditionService.existsNameAddition("von")).thenReturn(true);
+        when(nameAdditionService.existsNameAddition("von")).thenReturn(true);
 
-    // Test
-    ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
+        // Test
+        final ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
 
-    // Verify
-    assertConversion(csvPersonData, null, "Thomas", "von", "Gostomski", "tgostomski");
-  }
+        // Verify
+        assertConversion(csvPersonData, null, "Thomas", "von", "Gostomski", "tgostomski");
+    }
 
-  @Test
-  public void testConvertEtienneDeRuffray() {
-    // Prepare
-    final String personString = "Etienne de Ruffray (eruffray)";
+    @Test
+    public void testConvertEtienneDeRuffray() {
+        // Prepare
+        final String personString = "Etienne de Ruffray (eruffray)";
 
-    when(nameAdditionService.existsNameAddition("de")).thenReturn(true);
+        when(nameAdditionService.existsNameAddition("de")).thenReturn(true);
 
-    // Test
-    ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
+        // Test
+        final ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
 
-    // Verify
-    assertConversion(csvPersonData, null, "Etienne", "de", "Ruffray", "eruffray");
-  }
+        // Verify
+        assertConversion(csvPersonData, null, "Etienne", "de", "Ruffray", "eruffray");
+    }
 
-  @Test
-  public void testConvertDrDennisKrannich() {
-    // Prepare
-    final String personString = "Dr. Dennis Krannich (dkrannich)";
+    @Test
+    public void testConvertDrDennisKrannich() {
+        // Prepare
+        final String personString = "Dr. Dennis Krannich (dkrannich)";
 
-    when(titleService.existsTitleByName("Dr.")).thenReturn(true);
+        when(titleService.existsTitleByName("Dr.")).thenReturn(true);
 
-    // Test
-    ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
+        // Test
+        final ImportCsvParser.CsvPersonData csvPersonData = personStringConverter.convert(personString);
 
-    // Verify
-    assertConversion(csvPersonData, "Dr.", "Dennis", null, "Krannich", "dkrannich");
-  }
+        // Verify
+        assertConversion(csvPersonData, "Dr.", "Dennis", null, "Krannich", "dkrannich");
+    }
 
-  private void assertConversion(
-      final ImportCsvParser.CsvPersonData csvPersonData,
-      final String title,
-      final String firstName,
-      final String nameAddition,
-      final String lastName,
-      final String ldapUser) {
-    assertNotNull(csvPersonData);
+    private void assertConversion(
+        final ImportCsvParser.CsvPersonData csvPersonData,
+        final String title,
+        final String firstName,
+        final String nameAddition,
+        final String lastName,
+        final String ldapUser) {
+        assertNotNull(csvPersonData);
 
-    assertEquals(title, csvPersonData.getTitle());
-    assertEquals(firstName, csvPersonData.getFirstName());
-    assertEquals(nameAddition, csvPersonData.getNameAddition());
-    assertEquals(lastName, csvPersonData.getLastName());
-    assertEquals(ldapUser, csvPersonData.getLdapUser());
-  }
+        assertEquals(title, csvPersonData.getTitle());
+        assertEquals(firstName, csvPersonData.getFirstName());
+        assertEquals(nameAddition, csvPersonData.getNameAddition());
+        assertEquals(lastName, csvPersonData.getLastName());
+        assertEquals(ldapUser, csvPersonData.getLdapUser());
+    }
 }
